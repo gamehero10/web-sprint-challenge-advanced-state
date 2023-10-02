55654d7f6dc12b2c51ed03645fc687f9c27a71e4
Rafael Deluna <delunarafael@yahoo.com>
2023-10-02 00:26:48 -0400

    Completed Sprint 10 Challenge

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,36 +1,83 @@
+import { CHANGE_FALSE, CHANGE_QUESTION, CHANGE_TRUE, SET_SUCCESS_MESSAGE, MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_INFO_MESSAGE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, RESET_FORM } from "./action-types";
+import axios from "axios";
+
+
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+export function moveClockwise() {
+  return ({type: MOVE_CLOCKWISE})
+ }
 
-export function moveCounterClockwise() { }
+export function moveCounterClockwise() {
+  return ({type: MOVE_COUNTERCLOCKWISE})
+ }
 
-export function selectAnswer() { }
+export function selectAnswer(obj) {
+  return ({type: SET_SELECTED_ANSWER, payload: obj})
+ }
 
-export function setMessage() { }
+export function setMessage() {
+  return ({type: SET_INFO_MESSAGE})
+ }
 
-export function setQuiz() { }
+export function setQuiz() { 
+  return ({type: SET_QUIZ_INTO_STATE})
+}
 
-export function inputChange() { }
 
-export function resetForm() { }
+export function changeQuestion(text) {
+  return ({type: CHANGE_QUESTION, payload: text})
+}
+
+export function changeTrue(text) {
+  return ({type: CHANGE_TRUE, payload: text})
+}
+
+export function changeFalse(text) {
+  return ({type: CHANGE_FALSE, payload: text})
+}
+
+export function resetForm() { 
+  return ({type: RESET_FORM})
+}
+
+export function setSuccessMessage(successMessage) {
+  return ({type: SET_SUCCESS_MESSAGE, payload: successMessage})
+}
 
 // ❗ Async action creators
 export function fetchQuiz() {
   return function (dispatch) {
+    axios.get('http://localhost:9000/api/quiz/next')
+      .then(res => {
+        dispatch({type:SET_QUIZ_INTO_STATE, payload: res.data})
+      })
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
   }
 }
-export function postAnswer() {
+export function postAnswer(obj) {
   return function (dispatch) {
+    axios.post('http://localhost:9000/api/quiz/answer', obj)
+      .then(res => {
+        dispatch({type: SET_INFO_MESSAGE, payload: res.data.message});
+        dispatch(fetchQuiz());
+        console.log(res.data);
+      })
     // On successful POST:
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
   }
 }
-export function postQuiz() {
+export function postQuiz(obj) {
   return function (dispatch) {
+    axios.post('http://localhost:9000/api/quiz/new', obj)
+      .then(res => {
+        dispatch({type: SET_INFO_MESSAGE, payload: `Congrats: "${res.data.question}" is a great question!`});
+        dispatch({type: RESET_FORM});
+        console.log(res); 
+      })
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,33 +1,117 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+import { combineReducers } from 'redux';
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, CHANGE_QUESTION, CHANGE_TRUE, CHANGE_FALSE, SET_SUCCESS_MESSAGE, RESET_FORM } from './action-types';
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
+  switch(action.type){
+    case(MOVE_CLOCKWISE):{
+      if (state === 5) {
+        return  state - 5
+      }
+      return state + 1
+    }
+    case(MOVE_COUNTERCLOCKWISE):{
+      if (state === 0) {
+        return state + 5
+      }
+      return state - 1
+    }
+  }
   return state
 }
 
-const initialQuizState = null
+const initialQuizState = null;
 function quiz(state = initialQuizState, action) {
-  return state
+  switch(action.type){
+    case(SET_QUIZ_INTO_STATE): {
+      const newQuiz = {
+        question: action.payload.question,
+        answers: action.payload.answers,
+        quiz_id: action.payload.quiz_id,
+        answer_ids: [action.payload.answers[0].answer_id, action.payload.answers[1].answer_id]
+      }
+      return {
+        ...state,
+        quiz: newQuiz
+      }
+    }
+    
+  }
+  return state;
 }
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch(action.type){
+    case(SET_SELECTED_ANSWER): {
+      const selectedAnswer = {
+        quiz_id: action.payload.quiz_id,
+        answer_id: action.payload.answer_id
+      }
+      return selectedAnswer;
+    }
+  }
+  return state;
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch(action.type){
+    case(SET_INFO_MESSAGE): {
+      return action.payload;
+    
+    }
+    case(SET_SELECTED_ANSWER): {
+      const defaultMessage = '';
+      return defaultMessage;
+    }
+  }
+  return state;
 }
 
 const initialFormState = {
   newQuestion: '',
   newTrueAnswer: '',
   newFalseAnswer: '',
+  successMessage: ''
 }
 function form(state = initialFormState, action) {
-  return state
+  switch(action.type){
+    case(CHANGE_QUESTION):{
+      return {
+        ...state,
+        newQuestion: action.payload,
+      }
+    }
+    case(CHANGE_TRUE):{
+      return {
+        ...state,
+        newTrueAnswer: action.payload,
+      }
+    }
+    case(CHANGE_FALSE):{
+      return {
+        ...state,
+        newFalseAnswer: action.payload,
+      }
+    }
+
+    case(RESET_FORM):{
+      return initialFormState;
+    }
+    //case(SET_SUCCESS_MESSAGE): {
+      // const successMessage = `Congrats: "${action.payload}" is a great question!`;
+      /*return {
+        ...state,
+        newQuestion: '',
+        newTrueAnswer: '',
+        newFalseAnswer: '',
+        successMessage: `Congrats: "${action.payload}" is a great question!`
+      }
+    } */
+  }
+  return state;
 }
 
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
+export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form });
